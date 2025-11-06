3d90f43f5279b56d3d0a439c62fb63a89537ce04
Mustafa aljanabi <aljanmus@sheridancollege.ca>
2025-11-05 21:37:52 -0500

    22222

--- src/main/java/midtermgitcode2/gittttttttttt/Gittttttttttt.java
+++ src/main/java/midtermgitcode2/gittttttttttt/Gittttttttttt.java
@@ -11,6 +11,6 @@ package midtermgitcode2.gittttttttttt;
 public class Gittttttttttt {
 
     public static void main(String[] args) {
-        System.out.println("11111111");
+        System.out.println("2222222");
     }
 }
