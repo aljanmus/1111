a45d695645f58e2fb5bedda09bb3b6270bf9c776
Mustafa aljanabi <aljanmus@sheridancollege.ca>
2025-11-05 21:23:47 -0500

    11111

---
+++ src/main/java/midtermgitcode2/gittttttttttt/Gittttttttttt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package midtermgitcode2.gittttttttttt;
+
+/**
+ *
+ * @author musta
+ */
+public class Gittttttttttt {
+
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
